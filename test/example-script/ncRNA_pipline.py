4a38d6384c10df483bd23117afa330220fbf0287
xieguiyan <932617527@qq.com>
2021-09-01 20:50:27 +0800

    config2json

--- test/example-script/ncRNA_pipline.py
+++ test/example-script/ncRNA_pipline.py
@@ -8,16 +8,16 @@ import argparse
 import datetime
 import os
 import subprocess
-import sys
 import tempfile
 import re
+import json
 
 
 def println(command, script):
     print(command, file=script)
 
 
-def get_config_env(config_file):
+def get_config_env(config_file, workdir):
     config_env = {}
     with open(config_file) as cf:
         for i in cf:
@@ -26,6 +26,10 @@ def get_config_env(config_file):
             if i.find("=") > 1:
                 fields = [j.strip() for j in i.strip().split("=")]
                 config_env[fields[0]] = fields[1]
+    config_json = json.dumps(config_env, indent=4)
+    config_file_name = "{workdir}/config_transfered.json".format(workdir=workdir)
+    with open(config_file_name, "w") as json_file:
+        json_file.write(config_json)
     return config_env
 
 
@@ -35,7 +39,6 @@ def get_fastq_quals(filename, config_env):
     dump_cmd = [config_env["fastqdump"], "-I", "-X", "1000", "-Z", "--split-spot", filename]
     temp_dump = tempfile.NamedTemporaryFile(delete=False)
     subprocess.run(dump_cmd, stdout=temp_dump, stderr=subprocess.PIPE)
-    print(" ".join(dump_cmd))
     temp_dump.close()
     temp_dump_lst = open(temp_dump.name, "r").readlines()
     fastq_lst = temp_dump_lst[1::4]
@@ -43,20 +46,14 @@ def get_fastq_quals(filename, config_env):
     if solid_flag_num > 100:
         quals_flag = "--dumpbase"
     os.unlink(temp_dump.name)
-    print(quals_flag)
     return quals_flag
 
 
 def generate_command(script, srafiles, workdir, sample, config_file, config_env):
     os.path.isdir(workdir) or os.makedirs(workdir)
-    println("cd {workdir}".format(workdir=workdir), script)
-    tempdir = tempfile.mkdtemp(
-        prefix="analysis.bowtie.", suffix=".{}".format(datetime.datetime.now().strftime("%Y-%m-%d")), dir=workdir
-    )
-    filter_params = "ILLUMINACLIP:{adpter}:{filter_params}".format(
-        adpter=config_env["adp_path"], filter_params=config_env["trimmomatic_sRNA_para"]
-    )
-    println("mkdir -p {temp} && cd {temp}".format(temp=tempdir), script)
+    tempdir = tempfile.mkdtemp(prefix="analysis.bowtie.", suffix=".{}".format(datetime.datetime.now().strftime("%Y-%m-%d")), dir=workdir)
+    filter_params = "ILLUMINACLIP:{adpter}:{filter_params}".format(adpter=config_env["adp_path"], filter_params=config_env["trimmomatic_sRNA_para"])
+    println("cd {temp}".format(temp=tempdir), script)
     println("echo 'start processing' > benchmarks.log && date >> benchmarks.log", script)
     println("echo 'start fastq-dump' >> benchmarks.log && date >> benchmarks.log", script)
     sra_lst = []
@@ -65,30 +62,16 @@ def generate_command(script, srafiles, workdir, sample, config_file, config_env)
         sra_reads = "{sra}_1.fastq.gz".format(sra=sra)
         quals = get_fastq_quals(srafile, config_env)
         println(
-            "{fastqdump} {quals} --gzip --split-files {srafile} 1>{sra}.dump.log 2>&1".format(
-                fastqdump=config_env["fastqdump"], quals=quals, srafile=srafile, sra=sra
-            ),
+            "{fastqdump} {quals} --gzip --split-files {srafile} 1>{sra}.dump.log 2>&1".format(fastqdump=config_env["fastqdump"], quals=quals, srafile=srafile, sra=sra),
             script,
         )
         sra_filter_reads_1 = "{sra}.fastq.filter.1.gz".format(sra=sra)
-        sra_filter_reads_2 = "{sra}.fastq.filter.fianl.gz".format(sra=sra)
-        sra_lst.append(sra_filter_reads_2)
         println(
             "java -jar -Xms8000m -Xmx8000m {trimmomatic} SE -threads {cpu_number} {sra_reads} {out_reads} {filter_params} -trimlog {out_reads}.log 1>{out_reads}_run.log 2>&1".format(
                 trimmomatic=config_env["trimmomatic"],
                 cpu_number=config_env["cpu_number"],
                 sra_reads=sra_reads,
                 out_reads=sra_filter_reads_1,
-                filter_params=filter_params,
-            ),
-            script,
-        )
-        println(
-            "java -jar -Xms8000m -Xmx8000m {trimmomatic} SE -threads {cpu_number} {sra_reads} {out_reads} {filter_params} -trimlog {out_reads}.log 1>{out_reads}_run.log 2>&1".format(
-                trimmomatic=config_env["trimmomatic"],
-                cpu_number=config_env["cpu_number"],
-                sra_reads=sra_filter_reads_1,
-                out_reads=sra_filter_reads_2,
                 filter_params=filter_params,
             ),
             script,
