b3981216fa2f2f71c3bc9e64522cc5e1b4fff16e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-09-03 11:30:49 +0800

    python setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="EVAtool",
+    version="0.1",
+    keyworkds="evatool",
+    description="EVAtool",
+    author="Dr. Xie and Dr. Liu",
+    author_email="xieguiyanathustdotedudotcn",
+    url="https://github.com/xieguiyan/EVAtool",
+    packages=find_packages(),
+    install_requires=["numpy"],
+    license="MIT",
+    platforms="any",
+)
