e6183ca5b1c173cef355fa8b0f48296e69da7e1f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-09-03 16:05:35 +0800

    fastq

--- src/evatool/utils/fastq.py
+++ src/evatool/utils/fastq.py
@@ -6,6 +6,8 @@
 # @DESCRIPTION:
 
 import os
+from pathlib import Path
+from evatool.utils.config import Config
 import sys
 import datetime
 import logging
@@ -27,32 +29,47 @@ logger.addHandler(handler)
 class Fastq(object):
     """Fastq class"""
 
-    def __init__(self, srafile, workdir, config_file):
-        self.srafile = srafile
-        self.workdir = workdir
-        self.config_file = config_file
+    def __init__(self, inputfile: Path, config: Config, outputdir: Path):
+        self.inputfile = inputfile
+        self.workdir = outputdir
+        self.config = config
 
     def __str__(self):
-        return "SRA file: {}\nWork diretory: {}\nConfig: {}".format(self.srafile, self.workdir, self.config_file)
+        return "SRA file: {}\nWork diretory: {}\nConfig: {}".format(self.inputfile, self.workdir, self.config_file)
 
-    def mkresult():
+    def is_sra(self):
+        return True if self.inputfile.suffix == ".sra" else False
+
+    def dump_fastq(self):
+        cmd = [self.config["fastqdump"], "--split-files", self.inputfile]
+        return subprocess.check_output(args=cmd)
+
+    def trim(self) -> None:
         pass
+
+    def process_fastq(self):
+        if self.is_sra():
+            rc = self.dump_fastq()
+            if rc == 0:
+                logger.info("Dump SRA file {} to fastq".format(self.inputfile))
+            else:
+                logger.info("Error in dump SRA file {} to fastq".format(self.inputfile))
+
+        self.trim()
 
     def transfq(self):
         resultdir = "{dir}/analysis.bowtie.{time}".format(dir=self.workdir, time=datetime.datetime.now().strftime("%Y-%m-%d"))
         isExist = os.path.isdir(resultdir)
         if not isExist:
             os.makedirs(resultdir)
-        else:
-            pass
         os.chdir(resultdir)
         with open(self.config_file, "r") as c:
             config_env = json.load(c)
         logger.info("Directory has been created!\n")
-        sra = os.path.splitext(os.path.basename(self.srafile))[0]
+        sra = os.path.splitext(os.path.basename(self.inputfile))[0]
         sra_reads = "{sra}_1.fastq.gz".format(sra=sra)
         quals = "--dumpbase"
-        transfq = "{fastqdump} {quals} --gzip --split-files {srafile} 1>{sra}.dump.log 2>&1".format(fastqdump=config_env["fastqdump"], quals=quals, srafile=self.srafile, sra=sra)
+        transfq = "{fastqdump} {quals} --gzip --split-files {inputfile} 1>{sra}.dump.log 2>&1".format(fastqdump=config_env["fastqdump"], quals=quals, inputfile=self.inputfile, sra=sra)
         runfq = subprocess.run(transfq, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
         if runfq.returncode == 0:
             logger.info("sucess in transfer sra to fastq!")
@@ -73,6 +90,3 @@ class Fastq(object):
         else:
             logger.info("error in trimmomatic for fastq!")
         return "{}\n".format(resultdir)
-
-    def test(self):
-        print(self.__str__())
