1b390bc9bb15827ede2aebb3d4c1061fd9144a32
xieguiyan <932617527@qq.com>
2021-09-04 16:17:02 +0800

    logger

--- src/evatool/utils/logger.py
+++ src/evatool/utils/logger.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 # -*- conding:utf-8 -*-
-# @AUTHOR: Chun-Jie Liu
-# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @AUTHOR: Gui-Yan Xie
+# @CONTACT: xieguiyan.at.gmail.com
 # @DATE: 2021-09-03 15:33:56
 # @DESCRIPTION:
 
+import logging
+
 
 class Logger(object):
-    def __init__(self, log_file):
-        self.log_file = log_file
-        self.log_file_handler = open(self.log_file, "w")
+    def __init__(self, testlog):
+        self.testlog = testlog
+        self.logtofile = self.mylogger()
 
-    def __del__(self):
-        self.log_file_handler.close()
+    def mylogger(self):
+        logger = logging.getLogger("my_logger")
+        logger.setLevel(logging.INFO)
+        fh = logging.FileHandler("/home/xiegy/github/EVAtool/src/evatool/utils/log.txt")
+        fh.setLevel(logging.INFO)
+        print(self)
 
-    def log(self, msg):
-        self.log_file_handler.write(msg + "\n")
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        fh.setFormatter(formatter)
+
+        logger.addHandler(fh)
+
+        logger.info(self)
+
+
+# Logger.mylogger("success in log functions!")
