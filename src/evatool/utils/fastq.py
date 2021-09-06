5f1b7931eb3aaa6cda77a55f4d25ebd5ebdb684e
xieguiyan <932617527@qq.com>
2021-09-06 20:54:49 +0800

    fq & trimm

--- src/evatool/utils/fastq.py
+++ src/evatool/utils/fastq.py
@@ -12,19 +12,18 @@ import subprocess
 
 
 class Fastq(object):
-    def __init__(self, inputfile: Path, outputdir: Path, config: Config, logger: Logger):
+    def __init__(self, inputfile: Path, outputdir: Path, config: Config):
         self.inputfile = Path(inputfile)
         self.outputdir = outputdir
         self.config = config
-        self.logger = logger
+        # self.logger = logger
         self.trimname = f"{self.inputfile.stem}.fastq.filter.1.gz"
-        # self.processfq = self.process_fastq()
 
     def is_sra(self):
         return True if self.inputfile.suffix == ".sra" else False
 
     def dump_fastq(self):
-        cmd = [self.config.config["fastqdump"], "--split-files", self.inputfile]
+        cmd = [self.config.config["fastqdump"], "--gzip", "--split-files", self.inputfile]
         print(cmd)
         return subprocess.run(cmd)
 
@@ -32,33 +31,24 @@ class Fastq(object):
         sra_filter_reads_1 = self.trimname
         filter_params = f"ILLUMINACLIP:{self.config.config['adp_path']}:{self.config.config['trimmomatic_sRNA_para']}"
         cmd = f"java -jar -Xms8000m -Xmx8000m {self.config.config['trimmomatic']} SE -threads {self.config.config['cpu_number']} {self.inputfile.stem}_1.fastq.gz {sra_filter_reads_1} {filter_params} -trimlog {sra_filter_reads_1}.log 1>{sra_filter_reads_1}_run.log 2>&1"
+        print(cmd)
         return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
 
     def process_fastq(self):
         if self.is_sra():
             rc = self.dump_fastq()
             if rc.returncode == 0:
-                self.logger(f"Dump SRA file {self.inputfile} to fastq")
+                Logger(message=f"Success in dump SRA file {self.inputfile} to fastq", logfile="/home/xiegy/github/EVAtool/src/evatool/utils/log.txt").mylogger
             else:
-                self.logger.logtofile(f"Error in dump SRA file {self.inputfile} to fastq")
+                Logger(message=f"Error in dump SRA file {self.inputfile} to fastq", logfile="/home/xiegy/github/EVAtool/src/evatool/utils/log.txt").mylogger
 
         runtrim = self.trim()
-        if runtrim == 0:
-            self.logger.logtofile(f"Trimm {self.inputfile.stem} fastq file")
+        if runtrim.returncode == 0:
+            Logger(message=f"Trimm {self.inputfile.stem} fastq file", logfile="/home/xiegy/github/EVAtool/src/evatool/utils/log.txt").mylogger
         else:
-            self.logger.logtofile(f"Error in trimm {self.inputfile.stem} fastq file")
+            Logger(message=f"Error in trimm {self.inputfile.stem} fastq file", logfile="/home/xiegy/github/EVAtool/src/evatool/utils/log.txt").mylogger
 
 
-fastq = Fastq(
-    inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra",
-    outputdir="/home/xiegy/github/EVAtool/test/example-data",
-    config=Config(),
-    logger=Logger(message="preocessed fastq!", logfile="/home/xiegy/github/EVAtool/src/evatool/utils/log.txt"),
-)
+# fastq = Fastq(inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra", outputdir="/home/xiegy/github/EVAtool/test/example-data", config=Config())
 
 # fastq.process_fastq()
-# print(fastq.trimname)
-# print(fastq.outputdir)
-# print(fastq.config.config)
-print(fastq.logger.logtofile)
-# fastq.outputdir / fastq.trimname
