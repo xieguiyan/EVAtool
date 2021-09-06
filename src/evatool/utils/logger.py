1386c63dd7c45044e2fea5586be917bdd8088f7e
xieguiyan <932617527@qq.com>
2021-09-06 19:38:29 +0800

    logger

--- src/evatool/utils/logger.py
+++ src/evatool/utils/logger.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 # -*- conding:utf-8 -*-
 # @AUTHOR: Gui-Yan Xie
-# @CONTACT: xieguiyan.at.gmail.com
+# @CONTACT: xieguiyan.at.hust.edu.cn
 # @DATE: 2021-09-03 15:33:56
 # @DESCRIPTION:
 
 import logging
+from pathlib import Path
 
 
 class Logger(object):
-    def __init__(self, testlog):
-        self.testlog = testlog
+    def __init__(self, message, logfile: Path):
+        self.message = message
+        self.logfile = logfile
         self.logtofile = self.mylogger()
 
     def mylogger(self):
         logger = logging.getLogger("my_logger")
         logger.setLevel(logging.INFO)
-        fh = logging.FileHandler("/home/xiegy/github/EVAtool/src/evatool/utils/log.txt")
+        fh = logging.FileHandler(self.logfile)
         fh.setLevel(logging.INFO)
-        print(self)
 
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         fh.setFormatter(formatter)
 
         logger.addHandler(fh)
 
-        logger.info(self)
+        logger.info(self.message)
 
 
-# Logger.mylogger("success in log functions!")
+# Logger("Success in log functions!", "/home/xiegy/github/EVAtool/src/evatool/utils/log.txt").logtofile
