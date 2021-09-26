a843eddc940c82b395985c41898ad091aa38910e
xieguiyan <932617527@qq.com>
2021-09-26 21:52:22 +0800

    genome map

--- evatool/utils/sam.py
+++ evatool/utils/sam.py
@@ -17,12 +17,48 @@ class SAM(object):
     def __init__(self, fastq: Fastq):
         self.fastq = fastq
         self.samdir = Path(fastq.inputfile)
+        self.tag_fa = f"{self.fastq.outputdir}/{self.fastq.inputfile.stem}.fa"
+
+    def mapped_genome_anno(self) -> None:
+        outputpre = f"{self.fastq.outputdir}/{self.fastq.inputfile.stem}"
+        cmd = [
+            self.fastq.config.config["bowtie"],
+            "-x",
+            self.fastq.config.config["genome_index"],
+            "-p",
+            self.fastq.config.config["cpu_number"],
+            self.fastq.config.config["bowtie_para_4_genome"],
+            "-f",
+            self.tag_fa,
+            "-S",
+            f"{outputpre}.genome.sam",
+            "2>",
+            f"{outputpre}.genome.bowtie.stat",
+            "&&",
+            self.fastq.config.config["samtools"],
+            "view -bS",
+            f"{outputpre}.genome.sam",
+            ">",
+            f"{outputpre}.genome.bam",
+            "&&",
+            self.fastq.config.config["samtools"],
+            "sort",
+            f"{outputpre}.genome.bam",
+            "-o",
+            f"{outputpre}.genome.sort",
+            "&&",
+            self.fastq.config.config["bedtools"],
+            "bamtobed -i",
+            f"{outputpre}.genome.sort.bam",
+            ">",
+            f"{outputpre}.genome.sort.bed",
+        ]
+        cmd = " ".join(cmd)
+        print(cmd)
+        return subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
     def ncRNA_map(self, ncrna) -> None:
         sample_input = self.fastq.inputfile.stem
-        tag_fa = f"{self.fastq.outputdir}/{self.fastq.inputfile.stem}.fa"
-        output_sam = f"{self.fastq.outputdir}/{sample_input}.{ncrna}.sam"
-        bowtie_stat = f"{self.fastq.outputdir}/{sample_input}.{ncrna}.bowtie.stat"
         cmd = [
             self.fastq.config.config["bowtie"],
             "-x",
@@ -31,11 +67,11 @@ class SAM(object):
             self.fastq.config.config["cpu_number"],
             self.fastq.config.config["bowtie_para_4_{0}".format(ncrna)],
             "-f",
-            tag_fa,
+            self.tag_fa,
             "-S",
-            output_sam,
+            f"{self.fastq.outputdir}/{sample_input}.{ncrna}.sam",
             "2>",
-            bowtie_stat,
+            f"{self.fastq.outputdir}/{sample_input}.{ncrna}.bowtie.stat",
         ]
         cmd = " ".join(cmd)
         print(cmd)
@@ -46,10 +82,14 @@ class SAM(object):
             self.fastq.log.log(f"Failed in align to {ncrna} reference!")
 
     def get_sam(self) -> None:
+        genome_map = self.mapped_genome_anno()
+        if genome_map.returncode == 0:
+            self.fastq.log.log("Success in align to genome reference!")
+        else:
+            self.fastq.log.log("Failed in align to genome reference!")
         ncrna_lst = ["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "scRNA"]
         p = Pool(cpu_count())
         for ncrna in ncrna_lst:
-            print(ncrna)
             p.apply_async(self.ncRNA_map, args=(ncrna,))
         p.close()
         p.join()
