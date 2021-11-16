3f84efed1de670ebf4b0629d2b5453df3903e625
xieguiyan <932617527@qq.com>
2021-11-16 16:22:40 +0800

    change rna lst

--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -11,7 +11,7 @@ def test_fastq():
     from evatool.utils.logger import Logger
 
     fastq = Fastq(
-        inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra", outputdir="/home/xiegy/github/EVAtool/test/tmp_result", config=Config(), log=Logger(), ncrna_lst=["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "scRNA"]
+        inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra", outputdir="/home/xiegy/github/EVAtool/test/tmp_result", config=Config(), log=Logger(), ncrna_lst=["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "YRNA"]
     )
     fastq.process_fastq()
 
