679518dea8c66bdccc116571145b33019c3a783f
xieguiyan <932617527@qq.com>
2021-11-19 10:45:01 +0800

    run evatool

---
+++ test/example-script/example_evatool.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- conding:utf-8 -*-
+# @AUTHOR: Gui-Yan Xie
+# @CONTACT: xieguiyan at hust dot edu dot cn
+# @DATE: 2021-11-18 21:41:27
+# @DESCRIPTION:
+
+
+#-i: sra file with path (required)
+#-o: output directory (required)
+#-c: configure file with path ( not required)
+#-n: ncRNA type list (not required)
+
+
+/home/xiegy/github/EVAtool/venv/bin/python \
+  /home/xiegy/github/EVAtool/evatool/main.py \
+  -i /home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra \
+  -o /home/xiegy/github/EVAtool/test/tmp_result \
+  -c /home/xiegy/github/EVAtool/evatool/resource/configure.json \
+  -n "miRNA" "rRNA" "tRNA" "piRNA" "snoRNA" "snRNA" "YRNA"
