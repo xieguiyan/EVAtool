d71002713037cc28cf47bf2295df558b59bedc22
xieguiyan <932617527@qq.com>
2021-11-22 22:26:47 +0800

    change cut-off

--- evatool/utils/tag.py
+++ evatool/utils/tag.py
@@ -53,7 +53,7 @@ class Tag(object):
                 reads_n += seq_count
                 tag_number = n + 1
                 tag_number = "t{0:0>8d}".format(tag_number)
-                if seq_count > 1:
+                if seq_count > self.fastq.config["tag_cut"]:
                     out_reads_n += seq_count
                     tf.write(f">{tag_number}\t{seq_count:d}\n{line}\n")
                     # tag_count[tag_number] = int(seq_count)
