4a3ae0dd925c3ef0a7fb2c248880c1f454f7fbd2
xieguiyan <932617527@qq.com>
2021-11-23 19:47:35 +0800

    add config dir

--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -11,7 +11,11 @@ def test_fastq():
     from evatool.utils.logger import Logger
 
     fastq = Fastq(
-        inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra", outputdir="/home/xiegy/github/EVAtool/test/tmp_result", config=Config(), log=Logger(), ncrna_lst=["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "YRNA"]
+        inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR8185773.sra",
+        outputdir="/home/xiegy/github/EVAtool/test/tmp_result",
+        config=Config(configfile="/home/xiegy/github/EVAtool/refs/reference_config.json"),
+        log=Logger(),
+        ncrna_lst=["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "YRNA"],
     )
     fastq.process_fastq()
 
