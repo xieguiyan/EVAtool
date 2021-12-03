defd166e74531ae9361da9f650e24d224dacdec8
xieguiyan <932617527@qq.com>
2021-12-03 22:14:51 +0800

    test example

--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -11,8 +11,8 @@ def test_fastq():
     from evatool.utils.logger import Logger
 
     fastq = Fastq(
-        inputfile="/home/xiegy/github/EVAtool/test/example-data/SRR10078125.sra",
-        outputdir="/home/xiegy/github/EVAtool/test/tmp_result/SRR10078125",
+        inputfile="/home/xiegy/github/EVAtool/test/example-data/example.fastq",
+        outputdir="/home/xiegy/github/EVAtool/test/tmp_result/example_fq",
         config=Config(configfile="/home/xiegy/github/EVAtool/refs/reference_config.json"),
         log=Logger(),
         ncrna_lst=["miRNA", "rRNA", "tRNA", "piRNA", "snoRNA", "snRNA", "YRNA"],
