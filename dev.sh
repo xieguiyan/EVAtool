ec0b0381009f4f55c38fbed6b345c511bac40424
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-12-10 15:58:20 -0500

    setup

---
+++ dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2021-12-10 15:30:12
+# @DESCRIPTION:
+
+# Number of input parameters
+# python3 -m pip install --upgrade build
+# python3 -m pip install --upgrade twine
+
+
+# python3 -m build
+
+python setup.py sdist bdist_wheel
+python3 -m twine upload --repository testpypi dist/*
+
+# python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps evatool
