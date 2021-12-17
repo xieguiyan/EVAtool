640e310fb6ddbca1bd936bcfbeb4ae3a26396b72
xieguiyan <932617527@qq.com>
2021-12-17 21:37:57 +0800

    top exp

--- evatool/utils/report.py
+++ evatool/utils/report.py
@@ -20,11 +20,11 @@ class Report:
         self.ncrna_list = ncrna_list
         self.samprefix = f"{self.outputdir}/{self.inputfile.stem}"
 
-    def generate_html(self, body, body2, num_rnas, stoptime, img_path, config, sam_info):
+    def generate_html(self, body, body2, num_rnas, all_top10, stoptime, img_path, config, sam_info):
         env = Environment(loader=FileSystemLoader(template_path))
         template = env.get_template("template_report.html")
         with open(f"{self.outputdir}/Report_result.html", "w+") as fout:
-            html_content = template.render(stop_time=stoptime, body=body, body2=body2, num_rnas=num_rnas, img_path=img_path, config=config, sam_info=sam_info)
+            html_content = template.render(stop_time=stoptime, body=body, body2=body2, num_rnas=num_rnas, all_top10=all_top10, img_path=img_path, config=config, sam_info=sam_info)
             fout.write(html_content)
 
     def load_readlen_data(self):
@@ -67,12 +67,26 @@ class Report:
             rna_count[i] = count
         return rna_count
 
-    # def load_top10_rnas(self):
-    #     top10_rnas = {}
-    #     for i in self.ncrna_list:
-    #         with open(f"{self.samprefix}.{i}.exp", "r") as exp_freg:
-    #             exp_info = exp_freg.readlines()
-    #             top10_rnas[i] = exp_info[1:11]
+    def load_top10_rna(self):
+        all_top10_info = []
+        for i in self.ncrna_list:
+            with open(f"{self.samprefix}.{i}.exp", "r") as exp_rna:
+                a = 0
+                for t in exp_rna:
+                    if a < 6:
+                        a = a + 1
+                        line = t.strip().split("\t")
+                        print(line)
+                        if line[0] != "GeneSymbol":
+                            ncrna_exp = {}
+                            ncrna_exp["rna"] = line[0]
+                            ncrna_exp["count"] = line[1]
+                            ncrna_exp["rpm"] = line[2]
+                            ncrna_exp["type"] = i
+                            all_top10_info.append(ncrna_exp)
+                    else:
+                        break
+        return all_top10_info
 
     def load_imgpath_info(self):
         img_path = {}
@@ -91,12 +105,12 @@ class Report:
         len_dis = self.load_readlen_data()
         num_rnas = self.load_num_rnas_info()
         all_map_info = self.load_ncrnatype_data()
+        all_top10 = self.load_top10_rna()
         time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         body.append(len_dis)
-
         body2 = all_map_info
         sam_info["sam_name"] = self.inputfile.stem
         sam_info["sam_path"] = self.outputdir
         sam_info["ncrna_list"] = self.ncrna_list
         img_path = self.load_imgpath_info()
-        self.generate_html(body, body2, num_rnas, time, img_path, self.config, sam_info)
+        self.generate_html(body, body2, num_rnas, all_top10, time, img_path, self.config, sam_info)
